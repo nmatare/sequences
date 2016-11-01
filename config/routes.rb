3280778d11b07e3321589fa97549fa4c3efcb726
nmatare <nmatare@chicagobooth.edu>
2016-11-01 12:13:22 -0500

    before db

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,7 @@
 Rails.application.routes.draw do
+
+	get("/all_guesses", { :controller => "guesses", :action => "index"})
+
+	get("/show_answer", { :controller => "guesses", :action => "answer"})
+
 end
