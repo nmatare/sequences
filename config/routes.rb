c73e1a5ec2193001145a4cea97d2b7e00ee6f7dc
nmatare <nmatare@chicagobooth.edu>
2016-11-01 17:08:18 -0500

    d

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
 	get("/all_guesses", { :controller => "guesses", :action => "index"})
-
 	get("/show_answer", { :controller => "guesses", :action => "answer"})
 
 end
